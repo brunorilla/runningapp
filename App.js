09df75da1f8b6d127c214720f66f290fda37d38d
luzvictoria-mp <luz.victoria@mercadolibre.com>
2021-10-31 18:43:27 -0300

    creacion pantalla welcome y botones

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Welcome from './components/welcome';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Vista Login</Text>
-      <StatusBar style="auto" />
+      {/* <StatusBar style="auto" /> */}
+      <Welcome />
     </View>
   );
 }
@@ -14,7 +15,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/button/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, Button, View, Text } from 'react-native';
+
+const CustomButton = ({ title, color }) => {
+    const buttonStyle = (color === 'main') ? styles.main : styles.secondary;
+    return (
+        <View style={buttonStyle}>
+            {/* Pendiente: sumar al Text el callback del onClick del texto según la función que se mande como parámetro */}
+            <Text style={styles.text}>{title}</Text>
+        </View>
+    );
+  }
+
+ const styles = StyleSheet.create({
+    main: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 16,
+        backgroundColor: '#d7f205',
+        color: '#262626',
+        borderRadius: 8,
+        width: 300,
+        height: 48,
+        marginBottom: 16,
+      },
+      secondary: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 16,
+        color: '#262626',
+        borderRadius: 8,
+        borderWidth: 2,
+        width: 300,
+        height: 48,
+      },
+      text: {
+        fontSize: 15,
+        fontWeight: "bold"
+      }
+  });
+
+  export default CustomButton;
